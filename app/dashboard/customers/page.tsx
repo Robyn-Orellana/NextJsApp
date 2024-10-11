189664cf6909d8690ec53ece7f07975ee80a7aa6
Robyn <robynorellana16@gmail.com>
2024-10-11 12:17:15 -0600

    Practica de nextjs terminada

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customer',
+};
+
 export default function Page(){
     return <p>Customemrs Page, este es el texto de la pagina customers</p>;
 }
